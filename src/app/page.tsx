db794a9de0be2f83d7253ebff8321c3811afff76
Matheus Nadai <matheus.nadai@hotmail.com>
2023-11-11 14:50:11 -0300

    finish mobile landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,117 @@
-import Image from 'next/image'
+"use client";
+
+import { Download } from "@phosphor-icons/react";
+import image360 from "../../public/360.jpg";
+import { saveAs } from "file-saver";
+import { AppleLogo, DownloadSimple } from "@phosphor-icons/react/dist/ssr";
 
 export default function Home() {
+  const handleDownload = () => {
+    // Caminho para a imagem dentro da pasta public
+    const imagePath = "/360.png"; // Atualize o caminho conforme necessário
+
+    try {
+      // Crie um link <a> temporário para iniciar o download
+      const link = document.createElement("a");
+      link.href = imagePath;
+      link.download = "360";
+
+      // Adicione o link ao DOM e clique nele para iniciar o download
+      document.body.appendChild(link);
+      link.click();
+
+      // Remova o link do DOM após o download
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("Erro ao baixar a imagem:", error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className={`flex w-full h-full min-h-screen p-4 bg-base-white`}>
+      <div
+        className={`flex bg-primary h-full w-full rounded-sm justify-center al bg-opacity-95 text-center bg-[url("/layered-waves-haikei.svg")] bg-cover`}
+      >
+        <div className="flex flex-col pt-4 px-4 justify-center items-center">
+          <p className="text-6xl text-base-white font-bold">Museu Ibiporã</p>
+          <span className="text-xl text-base-white font-bold mb-6 twe">
+            Acesse a exibição 3D do projeto
+          </span>
+          <span className="text-base-white text-center font-semibold mb-3">
+            Passo 1: baixar o arquivo da imagem
+          </span>
+          <button
+            onClick={handleDownload}
+            className="border-2 border-base-white min-w-[197px] p-2 flex items-center justify-center gap-2 rounded-sm font-bold text-base-white shadow-lg min-h-14"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <DownloadSimple size={24} color="#fff" />
+            Download
+          </button>
+
+          <div className="flex flex-col justify-center my-6 gap-3">
+            <span className="text-base-white text-center font-semibold">
+              Passo 2: baixar o aplicativo
+            </span>
+            <a
+              className="flex items-center justify-center text-base-white bg-base-black w-full h-14 rounded-md"
+              href="https://apps.apple.com/br/app/ricoh-theta/id1023254741"
+            >
+              <div className="mr-3">
+                <svg viewBox="0 0 384 512" width="30">
+                  <path
+                    fill="currentColor"
+                    d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
+                  ></path>
+                </svg>
+              </div>
+              <div>
+                <div className="text-xs">Download on the</div>
+                <div className="-mt-1 font-sans text-2xl font-semibold">
+                  App Store
+                </div>
+              </div>
+            </a>
+            <a
+              className="flex items-center justify-center w-full text-base-white bg-base-black rounded-md h-14"
+              href="https://play.google.com/store/apps/details?id=com.xojot.vrplayer&pli=1"
+            >
+              <div className="mr-3">
+                <svg viewBox="30 336.7 120.9 129.2" width="30">
+                  <path
+                    fill="#FFD400"
+                    d="M119.2,421.2c15.3-8.4,27-14.8,28-15.3c3.2-1.7,6.5-6.2,0-9.7  c-2.1-1.1-13.4-7.3-28-15.3l-20.1,20.2L119.2,421.2z"
+                  ></path>
+                  <path
+                    fill="#FF3333"
+                    d="M99.1,401.1l-64.2,64.7c1.5,0.2,3.2-0.2,5.2-1.3  c4.2-2.3,48.8-26.7,79.1-43.3L99.1,401.1L99.1,401.1z"
+                  ></path>
+                  <path
+                    fill="#48FF48"
+                    d="M99.1,401.1l20.1-20.2c0,0-74.6-40.7-79.1-43.1  c-1.7-1-3.6-1.3-5.3-1L99.1,401.1z"
+                  ></path>
+                  <path
+                    fill="#3BCCFF"
+                    d="M99.1,401.1l-64.3-64.3c-2.6,0.6-4.8,2.9-4.8,7.6  c0,7.5,0,107.5,0,113.8c0,4.3,1.7,7.4,4.9,7.7L99.1,401.1z"
+                  ></path>
+                </svg>
+              </div>
+              <div>
+                <div className="text-xs">GET IT ON</div>
+                <div className="-mt-1 font-sans text-xl font-semibold">
+                  Google Play
+                </div>
+              </div>
+            </a>
+          </div>
+          <span className="text-base-white text-center font-semibold">
+            Após baixar o aplicativo e o arquivo da imagem, abra a imagem no
+            aplicativo. Em seguida, será possível rotacionar a imagem para obter
+            uma visão em 360 graus da Sala Brasil no Museu Ibiporã!
+          </span>
+
+          <img src="/mockup-cell.png" alt="Logo GL" className="w-full h-full" />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
